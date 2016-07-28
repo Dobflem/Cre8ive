996cc97eeccf860e68357681032504874b942bf6
Russell Hickey <RHickeyJnr@gmail.com>
2016-07-28 21:05:10 +0100

    Minimal Plugin

---
+++ spec/app.js
@@ -0,0 +1,5 @@
+describe("Test", function() {
+    it("should do nothing", function() {
+        expect(1).toBe(1);
+    });
+});
---
+++ src/dobby.js
@@ -0,0 +1,26 @@
+(function($) {
+    
+    var options = {
+        "img-src" : "square.png",
+        "caption" : "Squared",
+        "tooltip" : "Created by Russell Hickey",
+        "author"  : "Russell Hickey",
+        "description" : "Shows Square on screen"
+    };
+    
+    function drawSquare(plugin, paper) {
+        var x = paper.rect(10, 10, 10, 10);
+        x.animate({
+            transform: "T50,50"
+        }, 2000, function() {
+            plugin.repeat();
+        });
+    };
+    
+    $.Plugin(function(plugin) {
+        _plugin = plugin;
+        plugin.options(options);
+        plugin.click(drawDobby);
+    });
+    
+})(CRE8);
